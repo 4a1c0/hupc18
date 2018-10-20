3e63b42d048440e68c2942fafb717f2598a0a6ed
Pau Casacuberta Orta <pauapplecat@me.com>
2018-10-20 19:17:39 +0200

    Correccions del script

--- upload docker.sh
+++ upload docker.sh
@@ -4,6 +4,6 @@ export DOCKER_ID_USER="pauapplecat"
 
 docker login
 
-docker compose -t hupc18 .
-docker docker tag hupc18 $DOCKER_ID_USER/hupc18
+docker build -t hupc18 .
+docker tag hupc18 $DOCKER_ID_USER/hupc18
 docker push $DOCKER_ID_USER/hupc18
